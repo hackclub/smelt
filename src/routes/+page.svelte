994ebb506a86fa726bb150d22be8806da38b8a06
Sam Liu <sam@samliu.dev>
2025-07-10 19:20:23 -0400

    Update content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="min-h-screen bg-gray-300 flex items-center justify-center">
+<div class="min-h-screen bg-gray-300 flex relative items-center justify-center">
   <div class="flex flex-col space-y-12 items-center">
     <div class=" size-48 bg-orange-400 rounded-full"></div>
     <div class="text-center text-white bg-gray-500 px-8 py-2 -rotate-3">
@@ -14,7 +14,23 @@
   <div
     class="absolute left-0 top-0 hover:scale-105 motion-safe:transition-transform hover:rotate-30 duration-200 rotate-12"
   >
-    <a href="https://hackclub.com"><img src="flag_left.svg" class="h-20" /></a>
+    <a href="https://hackclub.com"
+      ><img src="/flag_left.svg" alt="Hack Club" class="h-20" /></a
+    >
   </div>
-  <div class="absolute"></div>
+  <div class="absolute flex justify-center left-0 right-0 bottom-0">
+    <div class="max-w-4xl w-full">
+      <img src="/crucible.svg" alt="Crucible" class="" />
+    </div>
+  </div>
+</div>
+
+<div class="bg-gray-500 flex justify-center">
+  <div class="w-full max-w-4xl">
+    <div>
+      <p class="text-white py-10 h-4 w-12 bg-orange-400 rounded-full">1</p>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
 </div>
