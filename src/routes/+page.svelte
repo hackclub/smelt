6356f7ad1a684c781ab0b35fdaaf4fcd400bff82
Sam Liu <sam@samliu.dev>
2025-07-10 16:38:50 -0400

    .
    Co-authored-by: avandoren <vandorena@users.noreply.github.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="min-h-screen bg-gray-300 flex items-center justify-center">
+  <div class="flex flex-col space-y-12 items-center">
+    <div class=" size-48 bg-orange-400 rounded-full"></div>
+    <div class="text-center text-white bg-gray-500 px-8 py-2 -rotate-3">
+      <p>
+        Ship a <span class="text-orange-300">Svelte/SvelteKit</span> website
+      </p>
+      <p>Get a <span class="text-orange-300">Svelted Coin</span></p>
+    </div>
+  </div>
+  <div
+    class="absolute left-0 top-0 hover:scale-105 motion-safe:transition-transform hover:rotate-30 duration-200 rotate-12"
+  >
+    <a href="https://hackclub.com"><img src="flag_left.svg" class="h-20" /></a>
+  </div>
+  <div class="absolute"></div>
+</div>
