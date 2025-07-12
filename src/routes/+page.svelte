884b094f182c517a6e62291c8831049d80333154
vandorena <alexvd@hackclub.com>
2025-07-12 18:35:20 -0400

    coiny

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
     style="background-image: url('/section1background.png')"
   >
     <div class="flex flex-col space-y-12 items-center">
-      <div class="size-80 bg-orange-400 rounded-full hover:drop-shadow-[0_10px_5px_rgba(57,58,76,0.5)]" data-tilt data-tilt-reset="true" data-tilt-scale="1.2" data-tilt-glare data-tilt-max-glare="0.6"></div>
+      <img src="/coin-flat.svg" alt="Svelted Coin" class="size-80 drop-shadow-[0_10px_5px_rgba(57,58,76,0.5)]" data-tilt data-tilt-reset="true" data-tilt-scale="1.2" data-tilt-glare data-tilt-max-glare="0.6" />
       <div
-        class="text-center text-white bg-gray-500 px-12 py-8 -rotate-3 text-3xl"
+        class="text-center text-white bg-gray-500 px-12 py-8 -rotate-3 text-3xl drop-shadow-[0_10px_5px_rgba(57,58,76,0.5)]"
       >
         <p>
           Ship a <a href="https://svelte.dev" class="text-orange-300 hover:underline">Svelte/SvelteKit</a> website
