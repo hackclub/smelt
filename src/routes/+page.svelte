a98904c2eab8011843f537b5dbb16d09ff4eae63
vandorena <alexvd@hackclub.com>
2025-07-14 11:22:25 -0400

    :) submit button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,8 +102,13 @@
       </div>
     </div>
   </div>
-  <div>
+  <div class="bg-gray-300 py-10 flex justify-center">
     <!-- This will be a Submission Section-->
+      <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+Pink to orange
+        </span>
+      </button>
   </div>
   <div>
     <!-- This will be a Carousel Showing Submissions -->
